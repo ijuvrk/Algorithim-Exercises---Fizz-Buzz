2c9f59516ab31433a9a81f17f08f0fd53a170790
ijuvrk <ijascochin@gmail.com>
2024-11-22 22:09:03 +0300

    removed terminator as it isn't necessary. it was a bug i encountered which made me use it.

--- Fizz Buzz.playground/Contents.swift
+++ Fizz Buzz.playground/Contents.swift
@@ -2,12 +2,12 @@ import UIKit
 
 for i in 1 ... 1000 {
     if i % 3 == 0 && i % 5 == 0 {
-        print("Fizz Buzz", terminator: "\n")
+        print("Fizz Buzz")
     }
     else if i % 3 == 0 {
-        print("Fizz", terminator: "\n")
+        print("Fizz")
     } else if i % 5 == 0 {
-        print("Buzz", terminator: "\n")
+        print("Buzz")
     } else {
         print(i)
     }
