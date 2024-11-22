ad0a9b37c49af2ea9fbbf69ee1c3ce9b093aa235
ijuvrk <ijascochin@gmail.com>
2024-11-22 22:01:16 +0300

    Solved the problem

--- Fizz Buzz.playground/Contents.swift
+++ Fizz Buzz.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+for i in 1 ... 1000 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("Fizz Buzz", terminator: "\n")
+    }
+    else if i % 3 == 0 {
+        print("Fizz", terminator: "\n")
+    } else if i % 5 == 0 {
+        print("Buzz", terminator: "\n")
+    } else {
+        print(i)
+    }
+}
